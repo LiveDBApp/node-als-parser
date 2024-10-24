7bb8bdc391de805fbac6f52a36dfc5b58f712394
Jeff Griffiths <jeff@eko-recordings.ca>
2024-10-23 20:41:53 -0700

    fixed tests

--- tests/liveproject.test.js
+++ tests/liveproject.test.js
@@ -1,26 +1,27 @@
 import { LiveProject, LiveSet } from '../index.js'
 import path from 'node:path'
 
-const TEST_PROJECT_FOLDER = path.resolve('./testfiles/Test Project')
-const TEST_PROJECT_NAME = 'Test Project'
+const TEST_PROJECT_PATH = path.resolve('./testfiles/Test Project')
+const TEST_PROJECT_FOLDER = 'Test Project'
+const TEST_PROJECT_NAME = 'Test'
 
 test('loading a project', async () => {
-	let _project = await new LiveProject(TEST_PROJECT_FOLDER)
-	expect(_project.path).toBe(TEST_PROJECT_FOLDER)
-	expect(_project.name).toBe('Test Project')
+	let _project = await new LiveProject(TEST_PROJECT_PATH)
+	expect(_project.path).toBe(TEST_PROJECT_PATH)
+	expect(_project.name).toBe('Test')
 	expect(_project.liveSetPaths.length).toBe(1)
 	expect(_project.liveSetPaths[0]).toBe(
-		`${TEST_PROJECT_FOLDER}/${TEST_PROJECT_NAME}.als`,
+		`${TEST_PROJECT_PATH}/${TEST_PROJECT_FOLDER}.als`,
 	)
 })
 
 test('loading sets in a project', async () => {
-	let _project = await new LiveProject(TEST_PROJECT_FOLDER)
+	let _project = await new LiveProject(TEST_PROJECT_PATH)
 
 	await _project.loadSets()
 
 	// console.log(_project.liveSets)
 
 	expect(_project.liveSets).toHaveLength(1)
-	expect(_project.liveSets[0].info.name).toBe(`${TEST_PROJECT_NAME}.als`)
+	expect(_project.liveSets[0].info.name).toBe(`${TEST_PROJECT_FOLDER}.als`)
 })
